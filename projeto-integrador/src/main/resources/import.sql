82fe9e84bb1df2dd362e68102e75075155bf77b7
CazulaDev <271841@unifio.edu.br>
2025-09-05 22:35:50 +0000

    Entidades

---
+++ projeto-integrador/src/main/resources/import.sql
@@ -0,0 +1,7 @@
+insert into Categoria (nome) values ('Bebidas');
+insert into Categoria (nome) values ('Carnes');
+
+insert into Fabricante (nome) values ('JBS');
+insert into Fabricante (nome) values ('The Coca-Cola Company');
+
+insert into Produto (nome, descricao, preco, categoria_id, fabricante_id) values ('Coca-Cola', 'Refrigerante de cola', 5.99, 1, 2);
